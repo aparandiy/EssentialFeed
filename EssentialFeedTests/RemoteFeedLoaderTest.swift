fb3a6bc7820e307cc6cb04737651aeb1003bf0d9
Andriy <aparandiy@mail.ru>
2023-12-14 19:02:57 +0200

    Moved test logic to a separate type HTTPClientSpy

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -8,28 +8,46 @@
 import XCTest
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://url.com")
+        HTTPClient.shared.get(from: URL(string: "https://url.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
     ///this is a private init which prevents HTTPClient fo being created by someone more than once, so if someone needs a HTTPClient he can call anly a shared property and can't use let client = HTTPClient(). this is a real singletone "S"
-    private init() {}
+//    static let shared = HTTPClient()
+//    private init() {}
+    
+    
+    ///this is not a singlton, because static var an opportunity to create a new instance with HTTPClient() and override the current static shared instance like
+    ///let newClient = HTTPClient()
+    ///HTTPClient.shared = newClient
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) { }
+}
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url: URL) {
+        requestedURL = url
+    }
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        ///static var instance of shared HTTPClient object gives us an opportunity to rewrite it with a subclass wich is our HTTPSClientSpy
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         sut.load()
         
