684a1d1798b35da03c96732383a0216e0180cb4e
Andriy <aparandiy@mail.ru>
2023-12-15 17:28:51 +0200

    Improve readability with helper method

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -62,8 +62,8 @@ class RemoteFeedLoaderTests: XCTestCase {
             requestedURLs.append(url)
         }
         
-        func complete(withStatusCode code:Int, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)
+        func complete(with error:Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 
