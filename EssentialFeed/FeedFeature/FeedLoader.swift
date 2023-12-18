ba22ae38064ceb8bded230668f04e326396541d9
Andriy <aparandiy@mail.ru>
2023-12-18 17:31:41 +0200

    Make the FeedLoader protocol public

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -13,6 +13,6 @@ public enum LoadFeedResult{
 }
 
 ///this is an interface between UI module and concrete implementation of FeedLoader. It's like a guideline
-protocol FeedLoader {
+public protocol FeedLoader {
     func load(completion:@escaping (LoadFeedResult)->())
 }
