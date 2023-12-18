0f06e65df28006604013b06657dc9324441cbbd0
Andriy <aparandiy@mail.ru>
2023-12-18 17:21:07 +0200

    Remove LoadFeedResult Equatable conformance from production since it's a test-only requirement

--- EssentialFeedTests/FeedAPI/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedLoaderTest.swift
@@ -152,17 +152,32 @@ class RemoteFeedLoaderTests: XCTestCase {
         let itemsJSON = ["items" : items]
         return try! JSONSerialization.data(withJSONObject: itemsJSON)
     }
-    
-    private func expect(_ sut: RemoteFeedLoader, toCompleteWith result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt =  #line) {
+    private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+        let exp = expectation(description: "Wait for load completion")
+
+         sut.load { receivedResult in
+
+             switch (receivedResult, expectedResult) {
+
+             case let (.success(receivedItems), .success(expectedItems)):
+                 XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+
+             case let (.failure(receivedError), .failure(expectedError)):
+                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+
+             default:
+                 XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+             }
+
+             exp.fulfill()
+         }
         
         action()
-        
-        XCTAssertEqual(capturedResults, [result], file: file, line: line )
+
+        wait(for: [exp], timeout: 1.0)
     }
-        
+            
     private class HTTPClientSpy: HTTPClient {// this is a class just for test, we are not using it somewhere else, so we moved it to the test scope and made it private
         //MARK: - Properties
         private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
