1ec09019091a5b47c7d955ef3ad637860eaa9fe0
Andriy <aparandiy@mail.ru>
2024-01-10 15:08:28 +0200

    Clean up & undo all cache side-effects on setUp and tearDown to avoid sharing state between tests

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -9,6 +9,18 @@ import XCTest
 import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+
+        setupEmptyStoreState()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
 
@@ -24,24 +36,37 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
-     }
+    }
 
-     // MARK: Helpers
-     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
-         let storeBundle = Bundle(for: CoreDataFeedStore.self)
-         let storeURL = testSpecificStoreURL()
-         let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
-         let sut = LocalFeedLoader(store: store, currentDate: Date.init)
-         trackForMemoryLeaks(store, file: file, line: line)
-         trackForMemoryLeaks(sut, file: file, line: line)
-         return sut
-     }
+    // MARK: Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 
-     private func testSpecificStoreURL() -> URL {
-         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
-     }
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
 
-     private func cachesDirectory() -> URL {
-         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-     }
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+
 }
