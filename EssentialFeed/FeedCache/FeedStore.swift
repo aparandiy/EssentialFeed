c473144984aeb47431e90cda7a66f92b107663a0
Andriy <aparandiy@mail.ru>
2024-01-17 11:10:00 +0200

    Refactor CachedFeed type from enum to tuple since we can represent the absence of a value with an `Optional`

--- EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -8,16 +8,13 @@
 import Foundation
 
 
-public enum CachedFeed {
-    case empty
-    case found(feed: [LocalFeedImage], timestamp: Date)
-}
+public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
-    typealias RetrieveResult  = Result <CachedFeed, Error>
 
     typealias DeletionCompletion  = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
+    typealias RetrieveResult  = Result <CachedFeed?, Error>
     typealias RetrievalCompletion = (RetrieveResult) -> Void
     
     /// The completion handler can be invoked in any thread.
