be4170cbff7e2fcceb039ab4450cbb03f6461009
Andriy <aparandiy@mail.ru>
2024-01-19 18:08:48 +0200

    Move FeedViewController to production target

---
+++ EssentialFeediOS/FeedVC.swift
@@ -0,0 +1,37 @@
+//
+//  FeedVC.swift
+//  EssentialFeediOS
+//
+//  Created by Andrij Parandij on 19.01.2024.
+//
+
+import UIKit
+import EssentialFeed
+
+final public class FeedVC: UITableViewController {
+    public var loader: FeedLoader?
+
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+    }
+    
+    public override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
