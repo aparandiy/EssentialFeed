2a640bacc8a4d2aca779795768813478056243e7
Andriy <aparandiy@mail.ru>
2024-01-20 15:34:53 +0200

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Andrij Parandij on 20.01.2024.
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
--- EssentialFeediOS/FeedVC.swift
+++ EssentialFeediOS/FeedVC.swift
@@ -9,8 +9,9 @@ import UIKit
 import EssentialFeed
 
 final public class FeedVC: UITableViewController {
-    public var loader: FeedLoader?
-
+    private var loader: FeedLoader?
+    private var tableModel = [FeedImage]()
+    
     public convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
@@ -30,8 +31,32 @@ final public class FeedVC: UITableViewController {
     
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
+        loader?.load { [weak self] result in
             self?.refreshControl?.endRefreshing()
+
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
+//            switch result {
+//            case let .success(feed):
+//                self?.tableModel = feed
+//            case let .failure(error):
+//                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+//                self?.present(alert, animated: true)
+//            }
         }
+    }
+    
+    //MARK: - UITableViewDatasource
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
     }
 }
