e5a4f215b80b272e3a61158a8b0c6546d96dbe79
Andriy <aparandiy@mail.ru>
2024-01-22 17:55:57 +0200

    Extract FeedImageCell test helpers to a separate file

--- EssentialFeediOSTests/Controllers/FeedVCTests.swift
+++ EssentialFeediOSTests/Controllers/FeedVCTests.swift
@@ -389,35 +389,3 @@ private class FakeRefreshControl: UIRefreshControl {
         _isRefreshing = false
     }
 }
-
-private extension FeedImageCell {
-    
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-    
-    var isShowingLocation: Bool {
-        return !locationContainer.isHidden
-    }
-
-    var isShowingImageLoadingIndicator: Bool {
-        return feedImageContainer.isShimmering
-    }
-    
-    var isShowingRetryAction: Bool {
-        return !feedImageRetryButton.isHidden
-    }
-    
-    var locationText: String? {
-        return locationLabel.text
-    }
-
-    var descriptionText: String? {
-        return descriptionLabel.text
-    }
-    
-    var renderedImage: Data? {
-        return feedImageView.image?.pngData()
-    }
-}
-
