1f13c73c86251e05c0e68f12de950bc4c4093d40
Andriy <aparandiy@mail.ru>
2024-01-26 10:54:24 +0200

    FeedRefreshController added guar let self to the binded callback

--- EssentialFeediOS/FeedUI/Controllers/FeedRefreshController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedRefreshController.swift
@@ -22,8 +22,9 @@ public final class FeedRefreshController: NSObject {
     
     private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
         feedVM.onLoadingStateChange = { [weak self] isLoading in
-            if isLoading { self?.refreshIndicator.beginRefreshing() }
-            else {  self?.refreshIndicator.endRefreshing() }
+            guard let self = self else { return  }
+            if isLoading { self.refreshIndicator.beginRefreshing() }
+            else {  self.refreshIndicator.endRefreshing() }
         }
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
         return view
