a39cc11bb325ec419373dc8eaaaf09a4fbe3f8f1
Andriy <aparandiy@mail.ru>
2024-02-13 11:06:37 +0200

    FeedImagePresenter displays loading image on `didStartLoadingImageData`

--- EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
@@ -6,10 +6,38 @@
 //
 
 import XCTest
+import EssentialFeed
+
+struct FeedImageVM {
+    let description: String?
+    let location: String?
+    let image: Any?
+    let isLoading: Bool
+    let shouldRetry: Bool
+
+    var hasLocation: Bool {
+        return location != nil
+    }
+}
+
+protocol FeedImageView {
+    func display(_ model: FeedImageVM)
+}
 
 class FeedImagePresenter {
-    init(view: Any) {
+    private let view: FeedImageView
 
+    init(view: FeedImageView) {
+        self.view = view
+    }
+
+    func didStartLoadingImageData(for model: FeedImage) {
+        view.display(FeedImageVM(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false))
     }
 }
 
@@ -20,6 +48,22 @@ class FeedImagePresenterTests: XCTestCase {
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
+    
+    func test_didStartLoadingImageData_displaysLoadingImage() {
+        let (sut, view) = makeSUT()
+        let image = uniqueImage()
+
+        sut.didStartLoadingImageData(for: image)
+
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.isLoading, true)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertNil(message?.image)
+    }
+
 
     // MARK: - Helpers
 
@@ -31,8 +75,12 @@ class FeedImagePresenterTests: XCTestCase {
         return (sut, view)
     }
 
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: FeedImageView {
+        private(set) var messages = [FeedImageVM]()
+
+        func display(_ model: FeedImageVM) {
+            messages.append(model)
+        }
     }
 
 }
